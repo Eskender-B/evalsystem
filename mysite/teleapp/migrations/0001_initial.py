836e02333042c792e4c6e1450d7b8a374af697c5
Eskender <eskenderbesrat@gmail.com>
2018-06-23 19:29:43 +0300

    Add HStoreExtension config to initial migration

--- mysite/teleapp/migrations/0001_initial.py
+++ mysite/teleapp/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
+from django.contrib.postgres.operations import HStoreExtension
 
 class Migration(migrations.Migration):
 
@@ -14,6 +15,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        HStoreExtension(),
         migrations.CreateModel(
             name='Employee',
             fields=[
