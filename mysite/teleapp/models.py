8ff26f090db202ea4cbbfa413920711a5c0b5beb
Eskender <eskenderbesrat@gmail.com>
2018-06-24 15:37:11 +0300

    Create database schemas; Show Employee status

--- mysite/teleapp/models.py
+++ mysite/teleapp/models.py
@@ -5,8 +5,28 @@ from django.contrib.postgres.fields import HStoreField
 # Create your models here.
 
 class Employees(models.Model):
+
+	STATUS = (
+	('A', 'Admin'),
+	('M', 'Manager'),
+	('S', 'Supervisor'),
+	('C', 'Coach'),
+	('J', 'Junior Staff'),
+	)
+
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	status = models.CharField(max_length=1, choices=STATUS, default='N')
+	evaluator = models.ForeignKey('Employees', on_delete=models.CASCADE, default=1)
 	data = HStoreField()
 
 	def __str__(self):
-		self.user.username
+		return self.user.username
+
+
+class Questions(models.Model):
+	
+	question_text = models.CharField(max_length=200)
+	weight = models.IntegerField()
+
+	def __str__(self):
+		return question_text
